76da69d861135ba5696aca7aada3d8a4223a5267
Ovo446 <yuey1673@gmail.com>
2025-05-04 12:15:39 -0400

    Update ted_scraper.py

--- ted_scraper.py
+++ ted_scraper.py
@@ -1,9 +1,10 @@
-import requests                               # 用于发送网页请求
-from bs4 import BeautifulSoup                 # 用于解析 HTML 网页内容
+# ted_scraper.py
+import requests
+from bs4 import BeautifulSoup
 
-def get_ted_talks():                          # 定义函数，用于爬 TED 演讲
-    url = 'https://www.ted.com/talks'         # TED 演讲页面的网址
-    response = requests.get(url)              # 向网址发 GET 请求
-    soup = BeautifulSoup(response.text, 'html.parser')  # 用 html.parser 解析网页
-    titles = [a.text.strip() for a in soup.select('div.talk-link a.ga-link')[:5]]  # 抓前5个演讲标题
-    return {'talks': titles}                  # 返回结果作为 JSON 字典
+def get_ted_talks():
+    url = "https://www.ted.com/talks"
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, "html.parser")
+    titles = [tag.text.strip() for tag in soup.select("h4.h9")]
+    return {"talks": titles[:10]}  # 返回前10个演讲标题
