b19ceefcc4ec81bb154a59d91926b2c81e304aa3
Ovo446 <yuey1673@gmail.com>
2025-05-04 11:59:24 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ def create_app():                             # 定义一个函数叫 create_app
 
     app.config.from_object('config')          # 从 config.py 文件中读取设置（比如后面可能用数据库设置）
 
-    from .routes import main                  # 从当前目录下的 routes.py 中导入叫 main 的蓝图（页面逻辑模块）
+    from app.routes import api_bp                  # 从当前目录下的 routes.py 中导入叫 main 的蓝图（页面逻辑模块）
     app.register_blueprint(main)              # 把蓝图挂到主 app 上，让 app 知道有哪些网址接口
 
     return app                                # 返回这个 app 应用，让别人可以用它来启动网站
