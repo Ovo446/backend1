d36dc21e86b0b9cfea1eaf66eefd48bd6269b48a
Ovo446 <yuey1673@gmail.com>
2025-05-04 12:14:19 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from app import create_app                    # 从 app 包中导入创建应用的函数
+# run.py
+from app import create_app
 
-app = create_app()                            # 调用函数，创建 app 对象
+app = create_app()
 
-if __name__ == '__main__':                    # 如果直接运行这个文件
-    app.run()                                 # 启动 Flask 应用（默认本地调试模式）
+if __name__ == "__main__":
+    app.run(debug=True)
